9d1d5266ba759d918503d464322b23ccba60e272
Airley Gabriel <airley.dev@gmail.com>
2026-03-02 08:30:48 -0300

    ajustes no jwtauthconverter

--- auth-service/src/main/java/com/example/demo/security/JwtAuthConverter.java
+++ auth-service/src/main/java/com/example/demo/security/JwtAuthConverter.java
@@ -1,15 +1,41 @@
 package com.example.demo.security;
 
-import org.springframework.security.oauth2.jwt.Jwt;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 
 public class JwtAuthConverter implements Converter<Jwt, AbstractAuthenticationToken> {
-    
+
     @Override
     public AbstractAuthenticationToken convert(Jwt jwt) {
-        return new JwtAuthenticationToken(jwt);
+
+        Map<String, Object> realmAccess = jwt.getClaim("realm_access");
+
+        List<String> roles = List.of();
+
+        if (realmAccess != null) {
+            Object rolesObj = realmAccess.get("roles");
+
+            if (rolesObj instanceof List<?> list) {
+                roles = list.stream()
+                        .filter(String.class::isInstance)
+                        .map(String.class::cast)
+                        .toList();
+            }
+        }
+
+        Collection<SimpleGrantedAuthority> authorities = roles.stream()
+                .map(role -> "ROLE_" + role)
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+
+        return new JwtAuthenticationToken(jwt, authorities);
     }
 }
