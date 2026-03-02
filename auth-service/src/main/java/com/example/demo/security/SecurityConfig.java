ee86ddac8226456a8bde5bbed16d671f03f2050a
Airley Gabriel <airley.dev@gmail.com>
2026-03-02 09:46:42 -0300

    esqueci de colocar o httpmethod.post

--- auth-service/src/main/java/com/example/demo/security/SecurityConfig.java
+++ auth-service/src/main/java/com/example/demo/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.demo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,7 +16,9 @@ public class SecurityConfig {
         http
         .csrf(csrf -> csrf.disable())
         .authorizeHttpRequests(auth -> auth
-            .requestMatchers("/public/**", "/auth/cadastro/", "/auth/verificar-email").permitAll()
+            .requestMatchers("/public/**").permitAll()
+            .requestMatchers(HttpMethod.POST, "/auth/cadastro").permitAll()
+            .requestMatchers(HttpMethod.POST, "/auth/verificar-email").permitAll()
             .anyRequest().authenticated()
         )
         .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()));
